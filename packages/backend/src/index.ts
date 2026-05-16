59cfca1c86846344e4ff8b2d533ebbf4bac71b16
Yashraj Dighe <yashraj.dighe077@gmail.com>
2026-05-17 00:00:05 +0530

    setu github authentication

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -25,6 +25,7 @@ backend.add(import('@backstage/plugin-techdocs-backend'));
 
 // auth plugin
 backend.add(import('@backstage/plugin-auth-backend'));
+backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 // See https://backstage.io/docs/auth/guest/provider
