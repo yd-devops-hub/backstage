fc179be3619d7f9bbfac03990e6a6b99bb93f71a
Yashraj Dighe <yashraj.dighe077@gmail.com>
2026-05-17 00:33:38 +0530

    enable github integration with github app

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -35,6 +35,7 @@ backend.add(import('@backstage/plugin-catalog-backend'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
+backend.add(import('@backstage/plugin-catalog-backend-module-github'));
 
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
